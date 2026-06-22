9429094a4240f0cd089928f8c9d8881feff59431
sts-ssc <sschelling@bluewin.ch>
2026-06-22 22:37:48 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "Football Score Predictor" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
